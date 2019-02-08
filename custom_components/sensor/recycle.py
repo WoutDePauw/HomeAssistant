8ba53eb675e67a6fbc4e7eec08bce629e5771b24
Wout De Pauw <wout.depauw@student.odisee.be>
2019-02-08 19:17:12 +0100

    Added attributes to weathercloud sensor, added alpha version of recycle sensor

--- custom_components/sensor/recycle.py
+++ custom_components/sensor/recycle.py
@@ -12,26 +12,32 @@ sensor:
 from homeassistant.util import Throttle
 from homeassistant.const import (ATTR_ATTRIBUTION, CONF_NAME)
 from homeassistant.helpers.entity import Entity
+from datetime import datetime
 import requests
-
-BASE_URL = 'https://app.weathercloud.net/device/values?code='
-CONF_ID = 'station_id'
+BASE_URL = 'https://www.ophaalkalender.be/api/rides?housenumber=0&id='
+ID_URL = 'https://www.ophaalkalender.be/calendar/findstreets?query='
+CONF_POSTAL = 'zip_code'
+CONF_STREET = 'street_name'
 HEADERS = {'X-Requested-With':'XMLHttpRequest'}
-DEFAULT_ATTRIBUTION = 'Data provided by Weathercloud'
+TIMEFORMAT = '%Y-%m-%d'
+DEFAULT_ATTRIBUTION = 'Please Recyle'
 def setup_platform(hass, config, add_devices, discovery_info=None):
 	"""Setup the sensor platform."""
-	station_id = config.get(CONF_ID)
-	add_devices([Weathercloud(station_id)])
+	zip_code = config.get(CONF_POSTAL)
+	street = config.get(CONF_STREET)
+	add_devices([Recyle(zip_code, street)])
 
 
-class Weathercloud(Entity):
+class Recyle(Entity):
 	"""Representation of a Sensor."""
 
-	def __init__(self, station_id):
+	def __init__(self, zip_code, street):
 		"""Initialize the sensor."""
 		self._state = None
-		self._id = station_id
+		self._zipcode = zip_code
+		self._street = street
 		self._attrs = {ATTR_ATTRIBUTION: DEFAULT_ATTRIBUTION}
+		self.getID()
 		self.update()
 	@property
 	def device_state_attributes(self):
@@ -41,7 +47,7 @@ class Weathercloud(Entity):
 	@property
 	def name(self):
 		"""Return the name of the sensor."""
-		return 'Weathercloud sensor'
+		return 'Recycle sensor'
 
 	@property
 	def state(self):
@@ -53,6 +59,14 @@ class Weathercloud(Entity):
 		"""return station id"""
 		return self._id
 
+	def getID(self):
+		"""gets ID for calendar filtering"""
+		r=requests.get(ID_URL + str(self._street) + '&zipcode=' + str(self._zipcode) , verify=False, headers=HEADERS)
+		self._id = r.json()[0]['Id']
+		self._attrs.update({
+			'Id': self._id
+		})
+
 	def update(self):
 		"""Fetch new state data for the sensor.
 
@@ -60,11 +74,20 @@ class Weathercloud(Entity):
 		"""
 
 		#try:
-		r=requests.get(BASE_URL + str(self._id) , verify=False, headers=HEADERS)
-
+		r=requests.get(BASE_URL + str(self._id) + '&zipcode=' + str(self._zipcode) , verify=False, headers=HEADERS)
 		resp = r.json()
-		self._attrs.update(resp)
-		self._state = resp
+		events = {}
+		now = datetime.now()
+		counter = 0
+		for item in resp:
+			then = datetime.strptime(item['start'][:-15], TIMEFORMAT)
+			if ( then >= now):
+				events[counter] = item
+				counter += 1
+				if counter >= 10:
+					break
+		self._attrs.update(events)
+		"""self._state = resp"""
 		#except:
 		#	self._state = "could not get status"
 		#	return
